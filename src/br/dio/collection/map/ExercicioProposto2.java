9f4e18e4ea8bd61f1579c1ea951ba0aaf24b24ae
viniciuspariz <viniciuspariz@gmail.com>
2022-02-12 22:15:10 -0300

    Resolvendo os exemplos de Map

---
+++ src/br/dio/collection/map/ExercicioProposto2.java
@@ -0,0 +1,9 @@
+package br.dio.collection.map;
+
+/*
+Faça um programa que simule um lançamento de dados. Lance o dado 100 vezes e armazene.
+Depois, mostre quantas vezes cada valor foi conseguido.
+ */
+
+public class ExercicioProposto2 {
+}
